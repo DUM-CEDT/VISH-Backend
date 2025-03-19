3bab3d1c3aba640c36bd1c103110c283f4236f22
NAT SIRIRUANGBUN <nat.siriruangbun@gmail.com>
2025-03-19 19:46:26 +0700

    New Upload

--- server.js
+++ server.js
@@ -1,33 +1,40 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
+const express = require('express')
+const dotenv = require('dotenv')
+const connectDB = require('./config/db')
 
-dotenv.config({ path: './config/config.env' });
+dotenv.config({path:'./config/config.env'})
 
-connectDB();
+connectDB()
 
-const vish = require('./routes/vish');
-const user = require('./routes/user');
+const vish = require('./routes/vish')
+const user = require('./routes/user')
+
 const credit = require('./routes/credit');
 const merchandise = require('./routes/merchandise');
 const transaction = require('./routes/transaction');
-const yanTemplateImage = require('./routes/YanTemplateImage');
-const yanTemplate = require('./routes/YanTemplate');
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use('/api/yan/image', yanTemplateImage);
-app.use('/api/yan/template', yanTemplate);
+const yanTemplateImage = require('./routes/YanTemplateImage')
+const yanTemplate = require('./routes/YanTemplate')
+const YanSetName = require('./models/YanSetName')
+
+app.use(express.json())
+app.use('/api/yan/image', yanTemplateImage)
+app.use('/api/yan/template', yanTemplate)
+
 app.use('/api/credit', credit);
 app.use('/api/merchandise', merchandise);
 app.use('/api/transactions', transaction);
-app.use('/api/vish', vish);
-app.use('/api/user', user);
 
-app.get('/', (req, res) => {
-    res.status(200).json({ success: true, msg: 'Hello World' });
-});
+app.get('/', (req , res) => {
+    res.status(200).json({success : true, msg : 'Hello World'})
+})
 
-// Export app for Vercel Serverless Function
-module.exports = app;
+app.use('/api/vish', vish)
+app.use('/api/user', user)
+
+const PORT = process.env.PORT || 1234
+
+//module.exports = app;
+const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, 'mode on port', PORT))
